109d9f906dfad37fdfccfb7180e3847136b8647b
Yifan Cui <t-yifancui@microsoft.com>
2022-08-23 14:15:46 +0800

    .

---
+++ beachball.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    changehint: "🚨🚨 Run 'yarn change' to generate a change file 🚨🚨",
+    ignorePatterns: [
+        "**/node_modules",
+        "**/test/**",
+        "**/*.config.*",
+        "**/package.json",
+        "**/CHANGELOG.*",
+        "**/change/**",
+        "**/yarn.lock",
+        "**/*.ps1",
+        "**/*.psm1",
+        "**/*.sh",
+    ],
+};
